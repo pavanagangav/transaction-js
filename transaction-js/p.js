a8f0b397fea3b13effff474cd4f29bbc9dd1156d
Vallabhu PavanaGanga <140430476+pavanagangav@users.noreply.github.com>
2025-04-02 16:17:32 +0530

    Add files via upload

---
+++ transaction-js/p.js
@@ -0,0 +1,29 @@
+// alert("accept cookies")
+// function fun(){
+//     alert("its function")
+// }
+// function changeTxt(){
+//     document.getElementById('demo').innerHTML="its Js"
+// }
+// confirm("accept cookies")
+// function fun(){
+//     confirm("its function")
+// }
+// prompt("accept cookies")
+// var x= prompt("accept cookies")
+// console.log(x)
+// document.write(x)
+// function fun(){
+//         alert("its function")
+//     }
+//     setInterval(fun,5000)
+// function fun(){
+//     alert("its function")
+// }
+// setTimeout(fun,5000)
+function openMsg(){
+    msg.classList.add('show')
+}
+function closeMsg(){
+    msg.classList.remove('show')
+}
